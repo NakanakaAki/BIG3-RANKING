b448615c78055ca98d7b1dd7fcb756ca09257fd7
NakanakaAki <akitonakahara0918@gmail.com>
2023-03-20 23:49:18 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+
+  # パスワード半角英数混合のバリデーション
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  has_many :rankings
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+
+    context '新規登録ができる' do
+      it '全ての入力事項が、存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'ニックネームが空でなければ登録できる' do
+        @user.nickname = 'あああ'
+        expect(@user).to be_valid
+      end
+      it 'パスワードが6文字以上半角英数字であれば登録できる' do
+        @user.password = '123abc'
+        @user.password_confirmation = '123abc'
+        expect(@user).to be_valid
+      end
+
+
+    end
+
+    context '新規登録ができない' do
+      it 'ニックネームが空欄だと保存できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'メールアドレスが空欄だと保存できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'メールアドレスがすでに登録しているユーザーと重複していると保存できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'パスワードが空欄だと保存できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'パスワードが5文字以下だと保存できない' do
+        @user.password = '123ab'
+        @user.password_confirmation = '123ab'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '111111'
+        @user.password_confirmation = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'ａａａ111'
+        @user.password_confirmation = 'ａａａ111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it 'パスワード（確認用）が空欄だと保存できない' do
+        @user.password = '123abc'
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
+end
